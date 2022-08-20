5eabe8e293b953599d41d0721c6f2b70a9dbfe97
Tricky Licky <wangombevictor5@gmail.com>
2022-08-20 17:45:57 +0300

    complete assignmet

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
+// function multiply( num1 , num2){
+//     return num1 * num2;
+// }
+// multiply(31 * 2);
 
-// Write your code here
+let num1 = 31;
+let num2 = 2;
+
+let multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 34;
+let num4 = 5;
+
+let mod = num3 % num4;
+
+const max = Math.max(5,10,15,20)
